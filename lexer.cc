ae0299d32a5bc5bf38861a01ab14f7a91bd4a1ed
Eugene Obrezkov <ghaiklor@gmail.com>
2017-02-21 14:05:59 +0200

    Chapter 2: The Abstract Syntax Tree (AST)

--- lexer.cc
+++ lexer.cc
@@ -90,3 +90,64 @@ static int gettok() {
   LastChar = getchar();
   return ThisChar;
 }
+
+// Base class for all expression nodes
+class ExprAST {
+public:
+  virtual ~ExprAST() {}
+};
+
+// Expression class for numeric literals like "1.0"
+class NumberExprAST : public ExprAST {
+  double Val;
+
+public:
+  NumberExprAST(double Val) : Val(Val) {}
+};
+
+// Expression class for referencing a variable, like "a"
+class VariableExprAST : public ExprAST {
+  std::string Name;
+
+public:
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+// Expression class for a binary operator
+class BinaryExprAST : public ExprAST {
+  char Op;
+  std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+  BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS) : Op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+}
+
+// Expression class for function calls
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+  CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args) : Callee(Callee), Args(std::move(Args)) {}
+}
+
+// Represents the "prototype" for a function,
+// which captures its name, and its argument names
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+
+public:
+  PrototypeAST(const std::string &name, std::vector<std::string> Args) : Name(name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
+
+// Represents a function definition itself
+class FunctionAST {
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto, std::unique_ptr<ExprAST> Body) : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
